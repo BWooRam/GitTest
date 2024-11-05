fb00b9a7fab3ac204f10f62d6797d0f2bfd624c9
BWooRam <117805293+BWooRam@users.noreply.github.com>
2024-11-05 16:07:40 +0900

    Revert "update feature1"

--- app/src/main/java/com/hyundaiht/gittest/ui/theme/Color.kt
+++ app/src/main/java/com/hyundaiht/gittest/ui/theme/Color.kt
@@ -6,12 +6,6 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Pink81 = Color(0xFFEFB8C8)
-val Pink82 = Color(0xFFEFB8C8)
-val Pink83 = Color(0xFFEFB8C8)
-val Pink84 = Color(0xFFEFB8C8)
-val Pink85 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
